12d5ae60f661712b5c02fb9c272749385ed56231
Jessica Murphy <murphyrjessica@gmail.com>
2024-03-27 01:09:11 -0400

    Login/register frontend

--- client/src/routes/HomePage.js
+++ client/src/routes/HomePage.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 function HomePage() {
+
+  let navigate = useNavigate()
+
+  const handleLoginSelect = () => { 
+    navigate(`/login`)
+  }
+  const handleRegisterSelect = () => { 
+    navigate(`/register`)
+  }
+
   return (
     <div>
       HomePage
-      <a href="/login"> Login</a>
-      <a href="/register"> Register </a>
+      <button type="button" onClick={handleLoginSelect}>Login</button>
+      <button type="button" onClick={handleRegisterSelect}>Register</button>
     </div>
   )
 }
