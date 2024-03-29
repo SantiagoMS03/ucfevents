d61898a2bf0b1d7d64e16920c174934c70e0079f
Jessica Murphy <murphyrjessica@gmail.com>
2024-03-28 23:45:07 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import HomePage from "./routes/HomePage";
 import { EventsContextProvider } from "./context/EventsContext";
+import { RSOContextProvider } from "./context/RSOContext";
 import EventsPage from "./routes/events/EventsPage";
 import EventDetailPage from "./routes/events/EventDetailPage";
 import EditEventPage from "./routes/events/EditEventPage";
@@ -14,20 +15,22 @@ import SuperAdminRegisterPage from "./routes/user/SuperAdminRegisterPage";
 function App() {
   return (
     <EventsContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<HomePage/>}/>
-          <Route exact path="/events" element={<EventsPage/>}/>
-          <Route exact path="/events/:eventid" element={<EventDetailPage/>}/>
-          <Route exact path="/events/:eventid/edit" element={<EditEventPage/>}/>
-          <Route exact path="/newevent" element={<CreateEventPage/>}/>
-          <Route exact path="/login" element={<LogInPage/>}/>
-          <Route exact path="/register" element={<RegisterPage/>}/>
-          <Route exact path="/register/user" element={<UserRegisterPage/>}/>
-          <Route exact path="/register/admin" element={<AdminRegisterPage/>}/>
-          <Route exact path="/register/superadmin" element={<SuperAdminRegisterPage/>}/>
-        </Routes>
-      </BrowserRouter>
+      <RSOContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<HomePage/>}/>
+            <Route exact path="/events" element={<EventsPage/>}/>
+            <Route exact path="/events/:eventid" element={<EventDetailPage/>}/>
+            <Route exact path="/events/:eventid/edit" element={<EditEventPage/>}/>
+            <Route exact path="/newevent" element={<CreateEventPage/>}/>
+            <Route exact path="/login" element={<LogInPage/>}/>
+            <Route exact path="/register" element={<RegisterPage/>}/>
+            <Route exact path="/register/user" element={<UserRegisterPage/>}/>
+            <Route exact path="/register/admin" element={<AdminRegisterPage/>}/>
+            <Route exact path="/register/superadmin" element={<SuperAdminRegisterPage/>}/>
+          </Routes>
+        </BrowserRouter>
+      </RSOContextProvider>
     </EventsContextProvider>
   );
 }
