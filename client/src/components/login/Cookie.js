4d634d309432197c520d3711024596f8e99ae85c
Jessica Murphy <murphyrjessica@gmail.com>
2024-04-04 15:06:05 -0400

    Cookies

---
+++ client/src/components/login/Cookie.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function GetCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.startsWith(name + '=')) {
+            return cookie.substring(name.length + 1);
+        }
+    }
+    return null;
+}
+export default GetCookie;
--- client/src/routes/WelcomePage.js
+++ client/src/routes/WelcomePage.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useEffect } from "react-router-dom";
 import Header from'../components/Header.js';
+import GetCookie from '../components/login/Cookie.js';
 
 function WelcomePage() {
+
+  //const id = GetCookie("user_id");
+  // console.log(id)
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await EventFinder.get(`/${}`);
+  //       console.log(response);
+  //       setSelectedEvent(response.data.data);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+
     return (
       <div>
         <Header />
