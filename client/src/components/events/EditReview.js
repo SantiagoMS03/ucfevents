338dcb7f86cd3f37d60e85f68c225581e6dd2086
Jessica Murphy <murphyrjessica@gmail.com>
2024-04-05 19:24:53 -0400

    Comments button

---
+++ client/src/components/events/EditReview.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect, Fragment } from 'react'
+import EventFinder from '../../apis/EventFinder'
+import { useParams } from "react-router-dom";
+
+const EditReview = () => {
+//   const { id } = useParams();
+//   const [name, setName] = useState("");
+//   const [category, setCategory] = useState("");
+//   const [description, setDescription] = useState("");
+//   const [date, setDate] = useState("");
+//   const [length_minutes, setLength] = useState("");
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await EventFinder.get(`/${id}`);
+//         const eventData = response.data.data.event;
+//         setName(eventData.name);
+//         setCategory(eventData.category);
+//         setDescription(eventData.description);
+//         setDate(eventData.date);
+//         setLength(eventData.length_minutes);
+//       } catch (err) {
+//         console.log(err);
+//       }
+//     };
+
+//     fetchData();
+//   }, [id]);
+
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const updatedEvent = await EventFinder.put(`/${id}`, {
+//         name,
+//         category,
+//         description,
+//         date,
+//         length_minutes
+//       })
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   }
+
+//   return (
+//     <Fragment>
+//       <form action=''>
+//         <div>
+//           <input
+//             value={name}
+//             onChange={(e) => setName(e.target.value)}
+//             type='text'
+//             placeholder='name'
+//           />
+//         </div>
+//         <div>
+//           <input
+//             value={category}
+//             onChange={(e) => setCategory(e.target.value)}
+//             type='text'
+//             placeholder='category'
+//           />
+//         </div>
+//         <div>
+//           <input
+//             value={description}
+//             onChange={(e) => setDescription(e.target.value)}
+//             type='text'
+//             placeholder='description'
+//           />
+//         </div>
+//         <div>
+//           <input
+//             value={date}
+//             onChange={(e) => setDate(e.target.value)}
+//             type='date'
+//             placeholder='date'
+//           />
+//         </div>
+//         <div>
+//           <input
+//             value={length_minutes}
+//             onChange={(e) => setLength(e.target.value)}
+//             type='number'
+//             placeholder='length'
+//           />
+//         </div>
+//         <button
+//           onClick={handleSubmit}
+//           type='submit'
+//         >
+//         Confirm
+//         </button>
+//       </form>
+//     </Fragment>
+//   )
+}
+
+export default EditReview;
--- server/routes/reviewsrouter.js
+++ server/routes/reviewsrouter.js
@@ -20,4 +20,34 @@ router.post('/:eventid/reviews', async (req, res) => {
     }
 })
 
+// // update comment
+// router.put('/:eventid', async (req, res) => {
+//     try {
+//         console.log("here!")
+//         const { name, category, description, date, length_minutes, rso_id, visibility } = req.body;
+//         const query = "UPDATE events SET name = $1, category = $2, description = $3, date = $4, length_minutes = $5, , rso_id = $6, visibility = $7 WHERE event_id = $8 RETURNING *"
+//         const results = await db.query(query, [name, category, description, date, length_minutes, rso_id, visibility, req.params.eventid]);
+//         res.status(200).json({
+//         status: "success",
+//         data: {
+//             event: results.rows[0]
+//         }
+//         });
+//     } catch (err) {
+//         console.log(err);
+//     }
+//     })
+    
+//     // delete comment
+//     router.delete('/:eventid', async (req, res) => {
+//     try {
+//         const results = await db.query("DELETE FROM events WHERE event_id = $1", [req.params.eventid]);
+//         res.status(204).json({
+//         status: "success"
+//         });
+//     } catch (err) {
+//         console.log(err);
+//     }
+//     })
+
 module.exports = router;
