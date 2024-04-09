4285a69ade33f8aabe506c1a1ca91ca3634354ff
georgiewald <georgiewald@yahoo.com>
2024-04-09 01:26:49 -0400

    Changes to the header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -19,6 +19,11 @@ function Header() {
     navigate("/welcome");
   }
 
+  const handleRSOPage = () =>{
+    console.log("Loading RSO page")
+    navigate("/rsos");
+  }
+
   return (
     <>
       <div className="div">
@@ -27,15 +32,17 @@ function Header() {
           <button className="div-3-button" onClick ={handleHomePage}>Home page</button>
           <button className="div-4-button">Map</button>
           <div className="div-5">
-            <button className="div-6-button">RSO's</button>
+            <button className="div-6-button"onClick ={handleRSOPage}> RSO's</button>
             <div className="div-7">
               <div className="div-8">
                 <div className="dropdown">
                   <button className="div-9-button">Events</button>
                   <div className="dropdown-content">
-                  <Link to="/universities/:universityid/rsos/:rsoid/events">All Events</Link>
+                  <Link to="/events">All Events</Link>
                     <a href="#">Attending</a>
-                    <a href="#">RSO Events</a>
+                    <Link to="/rsoevents">RSO events</Link>
+                    <Link to="/publicevents">Public Events</Link>
+                    <Link to="/privateevents">Private Events</Link>
                   </div>
                 </div>
               </div>
