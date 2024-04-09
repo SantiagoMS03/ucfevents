9381523d0ac11b7f923e78c70edfa34e481203dc
georgiewald <georgiewald@yahoo.com>
2024-04-09 01:27:11 -0400

    Formatted the welcome page a little

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -4,6 +4,7 @@ import GetCookie from '../components/Cookie.js';
 import UserFinder from '../apis/UserFinder.js';
 import UniversityFinder from '../apis/UniversityFinder.js';
 import { UniversityContext } from '../context/UniversityContext.js'
+import './Welcome.css';
 
 const Welcome = () => {
   //const[unis, setUnis] = useState([])
@@ -25,16 +26,15 @@ const Welcome = () => {
   }, []);
 
     return (
-      <div>
-        <div>
-          {/* Your welcome page content here */}
-          <h1>Welcome to {unis.name}'s Registered Student Organization Page</h1>
-          <p>Location: {unis.location}</p>
-          <p>{unis.description}</p>
-          <p>Student Body Size: {unis.students}</p>
+        <div className="container">
+          <div>
+            <h1 className="title">Welcome to {unis.name}'s Registered Student Organization Page</h1>
+            <p className="info"><span className="bold large">Location:</span><br />{unis.location}</p>
+            <p className="info"><span className="bold large">Description:</span> <br />{unis.description}</p>
+            <p className="info"><span className="bold large">Student Body Size:</span> <br />{unis.students}</p> 
+          </div>
         </div>
-      </div>
-    );
+      );
   }
 
 export default Welcome
